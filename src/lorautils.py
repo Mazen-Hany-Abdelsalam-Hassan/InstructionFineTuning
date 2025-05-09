b54dbe505deb804085d223dbb89b85fd3b571c98
Mazen <mazenhany1912003@gmail.com>
2025-05-09 17:27:25 +0300

    Model preparation and LoRA integration

--- src/lorautils.py
+++ src/lorautils.py
@@ -1,45 +1,42 @@
 import torch
-import torch.nn  as nn
-
-class LinearWithLoRa(nn.Module):
-    def __init__(self, linear, rank=16, alpha=1.5):
-        """
-        This is the layer which will be tuned in lora
-        :param linear:  Original Linear layer since we will replace all linear layer with this layer
-        :param rank:  HyperParameters related to the lora
-        :param alpha: HyperParameters related to the lora
-        """
+from torch.nn import  Module
+import torch.nn as nn
+from config import SEED
+class LoRA_layer(Module):
+    def __init__(self, input_dim: int, output_dim: int, rank: int = 16,
+                 alpha: float = 1.5):
         super().__init__()
-
-        self.linear = linear
-        din, dout = self.linear.in_features, self.linear.out_features
-        self.loralayer = LoRALayer(din=din, dout=dout, rank=rank, alpha=alpha)
-
-    def forward(self, x):
-        return self.linear(x) + self.loralayer(x)
-
-
-class LoRALayer(nn.Module):
-    def __init__(self,  din = 768 ,dout = 768 ,rank = 16 , alpha  = 1.5):
-        super().__init__()
+        self.input_transformation = nn.Linear(in_features=input_dim,
+                                              out_features=rank, bias=False)
+        self.output_transformation = nn.Linear(in_features=rank
+                                               , out_features=output_dim, bias=False)
+        nn.init.zeros_(self.output_transformation.weight)
+        nn.init.kaiming_uniform_(self.input_transformation.weight, a=5 ** .5)
         self.alpha = alpha
-        self.A = nn.Parameter(data = torch.empty(size =  (din ,rank ) ) )
-        nn.init.kaiming_uniform_(self.A , a = 5**.5)
-        self.B = nn.Parameter(data = torch.zeros((rank , dout)))
+
     def forward(self, x):
-        return self.alpha * (x@self.A@self.B)
+        state = self.input_transformation(x)
+        return self.output_transformation(state) * self.alpha
 
 
-def ReplaceLinear(model:nn.Module , rank = 16 , alpha = 1.5):
-    """
-        Replace all the linear layer with LoRA layer
-    :param model: module to replace all linear layer with LinearWithLoRa
-    :param rank: HyperParameters related to the lora
-    :param alpha: HyperParameters related to the lora
-    :return:
-    """
-    for name,module in model.named_children():
-        if isinstance(module , nn.Linear):
-            setattr(model ,name , LinearWithLoRa(module ,rank = rank , alpha=alpha))
-        else :
-            ReplaceLinear(model= module , rank=rank , alpha= alpha)
+class Linear_with_LoRA(nn.Module):
+    def __init__(self, linear_layer: nn.Linear, rank: int = 16, alpha: float = 1.5):
+        super().__init__()
+        self.linear_layer = linear_layer
+        input_dim = self.linear_layer.in_features
+        output_dim = self.linear_layer.out_features
+        self.lora_layer = LoRA_layer(input_dim=input_dim
+                                     , output_dim=output_dim, rank=rank, alpha=alpha)
+
+    def forward(self, x):
+        return self.linaer_layer(x) + self.lora_layer(x)
+
+
+
+def Replace_Linear(model:Module , rank:int = 16 , alpha:float= 1.5):
+    for child_name , child_layer in model.named_children():
+        if isinstance(child_layer , nn.Linear):
+            replace_with = Linear_with_LoRA(child_layer ,rank = rank , alpha = alpha)
+            setattr(model , child_name , replace_with )
+        else:
+            Replace_Linear( model = child_layer , rank = rank , alpha = alpha)
---
+++ src/model_modefied.py
@@ -0,0 +1,34 @@
+import torch
+
+from lorautils import Replace_Linear
+from config import MODEL_CONFIGS , BASE_CONFIG
+from torch.nn import   Module
+from GPT2 import  GPTModel
+from  model_download import download
+
+class GPT_INSTRUCTION_FINE_TUNED(Module):
+    def __init__(self, model_variant: str = "L",
+                 rank: int = 16,
+                 alpha: float = 1.5,
+                 dropout: float = .0):
+        super().__init__()
+        self.rank = rank
+        self.alpha = alpha
+        save_dir = download(model_variant)
+        weights = torch.load(save_dir, weights_only=True)
+        model_config = MODEL_CONFIGS[model_variant]
+        model_config.update(BASE_CONFIG)
+        model_config["drop_rate"] = dropout
+        self.model = GPTModel(model_config)
+        self.model.load_state_dict(weights)
+        self._prepare_model()
+
+
+
+    def _prepare_model(self):
+        for _, layer in self.model.named_parameters():
+            layer.requirs_grad = False
+            Replace_Linear(model=self.model, rank=self.rank, alpha=self.alpha)
+
+    def forward(self, x):
+        return self.model(x)
